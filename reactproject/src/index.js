6f7354499832cc8c09a308f2e4992fdf226104c2
Sonuje7 <sonuje233@gmail.com>
2022-02-20 22:41:20 +0500

    reactproject

--- reactproject/src/index.js
+++ reactproject/src/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Greeting() {
-  return <h4>This is My First Component</h4>;
-}
+import './main.css';
 
-ReactDOM.render(<Greeting />, document.getElementById('root'));
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => {
+  return (
+    <img
+      src='https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._AC_SX368_.jpg'
+      alt='image'
+    />
+  );
+};
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>;
+};
+const Author = () => {
+  return <h4>by Amelia Hepworth</h4>;
+};
+ReactDOM.render(<BookList />, document.getElementById('root'));
